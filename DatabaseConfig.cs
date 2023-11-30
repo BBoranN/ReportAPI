0bf28e7b493ef86e9fe6c71288adb99c3e1d9ecb
Hüseyin Boran Kuşcu <92054300+BBoranN@users.noreply.github.com>
2023-11-30 10:05:18 +0300

    Delete DatabaseConfig.cs

--- DatabaseConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ReportApi
-{
-    public static class DatabaseConfig
-    {
-        public static string databaseConnectionString= "User ID=postgres;Password=password;Server=localhost;Port=5433;Database=postgres;Integrated Security=true;Pooling=true; ";
-    }
-}
